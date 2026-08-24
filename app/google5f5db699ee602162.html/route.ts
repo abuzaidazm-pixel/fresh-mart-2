7e97276064090183d41790b2dc3952b6d4be3570
abuzaidazm-pixel <abuzaid.azm@gmail.com>
2026-08-25 03:21:56 +0530

    Delete app/google5f5db699ee602162.html directory

--- app/google5f5db699ee602162.html/route.ts
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-static";
-
-export function GET() {
-  return new Response(
-    "google-site-verification: google5f5db699ee602162.html",
-    { headers: { "Content-Type": "text/html" } }
-  );
-}
